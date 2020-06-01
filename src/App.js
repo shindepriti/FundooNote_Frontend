808be3aed33a0b6672ee68058428795bb97e3b19
shindepriti <pritishinde34@gmail.com>
2020-06-02 01:17:16 +0530

    REFACTORED:User Login Using Material UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Login from './components/login'
+import { BrowserRouter as Router, Route, } from 'react-router-dom';
+import Login from './components/login';
+
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Router>
+        <div>
+          <Route path="/login" component={Login} />
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/login.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import "../css/loginstyle.css"
+
+
+import Button from '@material-ui/core/Button';
+
+import Link from '@material-ui/core/Link';
+import { Card, TextField } from '@material-ui/core';
+import { Divider } from '@material-ui/core';
+class Login extends React.Component{
+    constructor(props){
+        super(props)
+            this.state={
+                email:'',
+                password:''
+            }
+    }
+    
+    handleChange=(event)=>{
+        this.setState({[event.target.name]: event.target.value});
+    }
+
+    handleSubmit(event) {
+        const { email, password } = this.state;
+    }
+
+    render(){
+        return (
+            <div className="container">            
+                <Card className="loginCard">
+                    <div className="login-header">
+                      Fundoo Login      
+                    </div>
+                    <div className="input-container">
+                        
+                        <TextField name="email" type="text"  label="Email" value={this.state.email} variant="outlined" onChange={this.handleChange} required />
+                        <TextField name="password" type="password" label="Password" value={this.state.password} variant="outlined" onChange={this.handleChange} required />
+                    </div>
+                    <div className="flex-container-row full-width">
+                        <div className="div-display">
+                            <Link component="button" variant="body2"> Forgot password ?</Link>
+                        </div>
+                        <div className="div-display">                        
+                            <Button variant="outlined" color="primary">Login</Button>
+                        </div>
+                    </div >                    
+                    <Divider className="divider-margin full-width"  variant="middle" />
+                    <div>
+                        <Button variant="outlined" size="small" color="secondary">Create New User</Button>
+                    </div>
+
+                </Card> 
+            </div>          
+          );
+    }
+
+}
+
+export default Login
